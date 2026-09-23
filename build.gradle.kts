b8fb49b8926933302338c855b123b9509ef211c0
Muhammet Ali SEZEN <bequeth112448@gmail.com>
2026-09-23 12:46:35 +0300

    Remove repositories configuration from allprojects block
    Removed allprojects block for repositories configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,6 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
     extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
